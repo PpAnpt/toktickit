cd3cb2c55c575afcdf1ca6c6bdd94b6d917a1155
PpAnpt <ppanapat1208@gmail.com>
2026-09-01 14:23:39 +0700

    Revert "Feature/5 automated tests"

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -4,6 +4,5 @@ export default defineConfig({
   test: {
     environment: "node",
     include: ["tests/**/*.test.ts"],
-    fileParallelism: false, // <--- เพิ่มบรรทัดนี้ เพื่อให้รันทีละไฟล์ ป้องกันปัญหาตั๋วชนกัน
   },
 });
